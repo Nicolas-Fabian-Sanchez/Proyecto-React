ec135efba5d5852b959208ed472ec1adb590e6cc
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2022-12-16 19:55:53 -0300

    primer mensaje

---
+++ src/components/ListItemHome/ListItemHome.js
@@ -0,0 +1,11 @@
+export default function ListItemHome ({dato,otraData,info}){
+  
+    const mostrar =()=>{
+        console.log(info)
+        alert(otraData)
+   }
+
+    return(
+        <li onClick={mostrar}>{dato}</li>
+    )
+} 
